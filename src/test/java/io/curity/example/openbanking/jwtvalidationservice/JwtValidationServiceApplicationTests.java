c6aea7bd1b4079216340a7adfa3b0e1a21335103
Judith Kahrer <judith@curity.io>
2021-06-24 14:26:20 +0200

    Working POC

---
+++ src/test/java/io/curity/example/openbanking/jwtvalidationservice/JwtValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.curity.example.openbanking.jwtvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = JwtValidationServiceApplication.class)
+class JwtValidationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
